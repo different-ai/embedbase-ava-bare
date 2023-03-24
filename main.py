2f4285fde565b9c8b25bb08471ab964b0756dbec
ben <ben@prologe.io>
2023-03-24 14:39:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,42 +13,9 @@ import logging
 settings = get_settings()
 MINIMUM_VERSION = os.getenv("MINIMUM_VERSION", "2.19.0")
 
-async def version_check(request: Request, call_next):
-    if any(path in request.scope["path"] for path in PRODUCTION_IGNORED_PATHS):
-        return await call_next(request)
-
-    # extract version from header
-    for name, value in request.scope["headers"]:  # type: bytes, bytes
-        if name == b"x-client-version":
-            client_version = value.decode("utf8")
-            break
-    else:
-        client_version = None
-
-    if not client_version:
-        return JSONResponse(
-            status_code=401,
-            content={"message": "missing client version"},
-        )
-
-    if semantic_version.Version(client_version) < semantic_version.Version(MINIMUM_VERSION):
-        message = "version too old, please update your plugin"
-        logging.info(message)
-        return JSONResponse(
-            status_code=401,
-            content={"message": message},
-        )
-
-    response = await call_next(request)
-
-    return response
-
-
 app = (
     get_app(settings)
     .use(Supabase(settings.supabase_url, settings.supabase_key))
-    .use(version_check)
-    .use(History)
     .use(
         CORSMiddleware,
         allow_origins=["*"],
